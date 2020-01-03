1ab1bee4d00454b81cfc2f064064c78d3fdf0763
AritaAlidemaj <58113020+AritaAlidemaj@users.noreply.github.com>
2020-01-03 12:01:55 +0100

    Update jquerythingz.js

--- javascript/jquerythingz.js
+++ javascript/jquerythingz.js
@@ -3,3 +3,31 @@ $(".fadeBody").fadeIn(1000);
 });
 
 
+$(document).ready(function(){
+    $("input").focus(function(){
+        $(this).css("background-color", "#752424");
+    });
+    $("input").blur(function(){
+        $(this).css("background-color","#ADADAD");
+    });
+    $("textarea").focus(function(){
+        $(this).css("background-color", "#752424");
+    });
+    $("textarea").blur(function(){
+        $(this).css("background-color","#ADADAD");
+    });
+    $("select").focus(function(){
+        $(this).css("background-color", "#752424");
+    });
+    $("select").blur(function(){
+        $(this).css("background-color", "#ADADAD");
+    });
+    $("button").focus(function () {
+        $(this).css("background-color", "#752424");
+    });
+    $("button").blur(function () {
+        $(this).css("background-color", "#ADADAD");
+    });
+});
+
+
